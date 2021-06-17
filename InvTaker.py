b434e841dcecb371ea183acf3109a8c9601cf66b
Terell-Davis <54454563+Terell-Davis@users.noreply.github.com>
2021-06-17 09:35:15 -0400

    to remove a upc first type '-' and scan the barcode

--- InvTaker.py
+++ InvTaker.py
@@ -5,16 +5,19 @@ while True:
     barcode = str(input("Scan barcode: "))
     # recognize stop code
     if barcode == "stop": break
-    # create key in dict if it does not exist
-    if barcode not in barcodes:
-        barcodes[barcode] = 0
-    # increment counter
-    barcodes[barcode] += 1
 
-# open up our file
+    if barcode.startswith("-"):
+        print("Removed")
+        barcodes[barcode[1:]] -= 1
+
+    if barcode not in barcodes and not barcode.startswith("-"):
+        barcodes[barcode] = 0
+
+    if not barcode.startswith("-"):
+        barcodes[barcode] += 1
+
 with open(section + ".txt", "a") as outputfile:
     # iterate over barcode dict
     for barcode, count in barcodes.items():
         # write line for each of these
         outputfile.write("UPC: {} Count: {}\n".format(barcode, str(count)))
-    # ending the with statement closes and saves the file automatically
